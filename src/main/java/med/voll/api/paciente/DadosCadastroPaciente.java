b3b209c72e4aea2ee176ad4abe7f90877033f65f
MariaEdSou <ma.edsousa00@gmail.com>
2023-05-08 21:08:07 -0300

    criacao de metodo POST de paciente

---
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,13 @@
+package med.voll.api.paciente;
+
+import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.medico.Especialidade;
+
+public record DadosCadastroPaciente(
+        String nome,
+        String telefone,
+        String email,
+        DadosEndereco endereco
+
+) {
+}
--- src/main/java/med/voll/api/service/MedicoService.java
+++
@@ -1,7 +0,0 @@
-package med.voll.api.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class MedicoService {
-}
