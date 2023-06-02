8b132e6bfea602710d08467706b326261ff72c2d
MariaEdSou <ma.edsousa00@gmail.com>
2023-06-02 19:18:53 -0300

    criando interface de validadorDeAgendamentoDeConsulta

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadaorAgendamentoDeConsulta.java
@@ -0,0 +1,8 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.consulta.DadosAgendamentoConsulta;
+
+public interface ValidadaorAgendamentoDeConsulta {
+
+    void validar(DadosAgendamentoConsulta dados);
+}
