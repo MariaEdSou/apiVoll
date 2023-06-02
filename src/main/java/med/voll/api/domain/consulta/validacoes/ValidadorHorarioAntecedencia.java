4eb4442ab4534fc86f09a351c3026ba14c5418fb
MariaEdSou <ma.edsousa00@gmail.com>
2023-06-01 21:38:25 -0300

    fazendo validacao de horario para agendamento de consulta

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,22 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.ValidacaoException;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioAntecedencia {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+
+        var dataConsulta = dados.data();
+        var agora = LocalDateTime.now();
+        var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
+
+        if (diferencaEmMinutos < 30) {
+            throw new ValidacaoException("Consulta deve ser agendada com antecedencvia minima de 30 minutos");
+        }
+
+    }
+}
